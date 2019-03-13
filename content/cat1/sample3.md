87f48604b8c916df3c6ac4f4ca94728b2aa5be32
Abraham Guimbao <abram.2197@gmail.com>
2019-03-13 18:51:58 +0100

    changed theme to Chunk

---
+++ content/cat1/sample3.md
@@ -0,0 +1,8 @@
+Title: Test Post 3
+Date: 1997-08-29 02:14
+Tags: testtag
+Author: My Name
+Summary: This is an article
+
+"Hope" is the thing with feathers -
+...
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,27 @@
+# custom page generated with a jinja2
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
 AUTHOR = u'Abraham Guimbao'
 SITENAME = u'abguimba'
-SITEURL = ''
+SITEURL = 'https://abguimba.github.io'
 
+THEME = 'themes/chunk'
+OUTPUT_PATH = 'output'
 PATH = 'content'
+
+#GITHUB_URL = 'https://github.com/abguimba'
+#MENUITEMS = True
+
+## Settings used by this theme (All Optional)
+DEFAULT_DATE_FORMAT = ('%d %b %Y')
+SITESUBTITLE = 'Portfolio'
+FOOTER_TEXT = ' '
+DISPLAY_CATEGORIES_ON_MENU = True
+SINGLE_AUTHOR = True
+MINT = False
+
 
 TIMEZONE = 'Europe/Paris'
 
@@ -21,7 +36,8 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Github', 'https://github.com/abguimba'),
-         ('Linkedin', 'https://www.linkedin.com/in/abraham-guimbao-parra/'))
+         ('Linkedin', 'https://www.linkedin.com/in/abraham-guimbao-parra/'),
+         ('Twitter', 'https://www.twitter.com/abguimba'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -29,5 +45,23 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
+STATIC_PATHS = [
+#    'images',
+    'extra',
+    'extra/robots.txt', 
+    'extra/favicon.ico',
+]
+EXTRA_PATH_METADATA = {
+#   'extra/custom.css': {'path': 'custom.css'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},  # and this
+#    'extra/CNAME': {'path': 'CNAME'},
+#    'extra/LICENSE': {'path': 'LICENSE'},
+#    'extra/README': {'path': 'README'},
+}
+
+
+TYPOGRIFY = True
+
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
